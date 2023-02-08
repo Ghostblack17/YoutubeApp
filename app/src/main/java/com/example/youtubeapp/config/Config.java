ed774912839b1d0744eed34d277a5d96d17777e1
GhostWing17 <ghostwing17@gmail.com>
2023-02-08 14:22:34 +0900

    Youtube Test

---
+++ app/src/main/java/com/example/youtubeapp/config/Config.java
@@ -0,0 +1,6 @@
+package com.example.youtubeapp.config;
+
+public class Config {
+    public static final String YOUTUBE_API_KEY = "&key=AIzaSyBOrW-mdfK6LcQblyHKhGsdoV8fFgbb8NU";
+
+}
---
+++ app/src/main/java/com/example/youtubeapp/model/Youtube.java
@@ -0,0 +1,13 @@
+package com.example.youtubeapp.model;
+
+public class Youtube {
+    public String videoId, title, description, thumbUrl, thumbUrlHigh;
+
+    public Youtube(String videoId, String title, String description, String thumbUrl, String thumbUrlHigh) {
+        this.videoId = videoId;
+        this.title = title;
+        this.description = description;
+        this.thumbUrl = thumbUrl;
+        this.thumbUrlHigh = thumbUrlHigh;
+    }
+}
